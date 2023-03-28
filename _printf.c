3e74d6fded46135066161a62f4d814ea9f31dbbf
Bosco <bizimana922@gmail.com>
2023-03-28 03:36:16 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,30 +1,120 @@
 #include "main.h"
 
 /**
- * _printf - Produces output according to a format
- * @format: Is a character string. The format string
- * is composed of zero or more directives
+ * _printf - prints a formatted string to the standard output stream
+ * @format: the format string to print
  *
- * Return: The number of characters printed (excluding
- * the null byte used to end output to strings)
- **/
+ * Return: the number of characters printed (excluding the null byte used to
+ *         end output to strings)
+ */
 int _printf(const char *format, ...)
 {
-	int size;
 	va_list args;
+	int i, j, count = 0;
+	print_t func_list[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_number},
+		{'i', print_number},
+		{'u', print_unsigned_number},
+		{'x', print_hexadecimal_lower},
+		{'X', print_hexadecimal_upper},
+		{'\0', NULL}
+	};
+
+	va_start(args, format);
 
 	if (format == NULL)
 		return (-1);
 
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		else
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			for (j = 0; func_list[j].type != '\0'; j++)
+			{
+				if (format[i + 1] == func_list[j].type)
+				{
+					count += func_list[j].func(args);
+					i++;
+					break;
+				}
+			}
+			if (func_list[j].type == '\0')
+			{
+				write(1, &format[i], 1);
+				count++;
+			}
+		}
+	}
 
-	va_start(args, format);
-	size = handler(format, args);
-
-	_putchar(-1);
 	va_end(args);
 
-	return (size);
+	return (count);
 }
+
+/**
+ * print_char - prints a character
+ * @args: the va_list argument
+ *
+ * Return: the number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: the va_list argument
+ *
+ * Return: the number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	return (write(1, str, _strlen(str)));
+}
+
+/**
+ * print_percent - prints a percent character
+ * @args: the va_list argument (unused)
+ *
+ * Return: the number of characters printed
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+
+	return (write(1, "%", 1));
+}
+
+/**
+ * print_number - prints a signed integer
+ * @args: the va_list argument
+ *
+ * Return: the number of characters printed
+ */
+int print_number(va_list args)
+{
+	char buffer[1024];
+	int i = 0, j = 0, n = va_arg(args, int);
+
+	if (n
+
--- main.h
+++ main.h
@@ -1,50 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * struct _format - Typedef struct
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
+ * struct print - struct for print functions
+ * @type: the type of the argument
+ * @func: the print function
+ */
+typedef struct print
 {
 	char type;
-	int (*f)(va_list);
-} format;
+	int (*func)(va_list);
+} print_t;
 
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_number(va_list args);
+int print_unsigned_number(va_list args);
+int print_hexadecimal_lower(va_list args);
+int print_hexadecimal_upper(va_list args);
 
 #endif /* MAIN_H */
+
