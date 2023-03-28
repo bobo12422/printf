d665e4ecacb2a50bb6dfea1e687485ffc4c337c0
vicano-code <vc.anokwuru@gmail.com>
2023-03-28 06:48:56 -0700

    correct error code on some ormat specifiers

--- _printf.c
+++ _printf.c
@@ -1,3 +1,6 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,26 +12,49 @@
 
 int _strlen(char *s)
 {
-int len = 0;
+	int len = 0;
 
-while (*s++)
-len++;
+	while (*s++)
+	len++;
 
-return (len);
+	return (len);
+}
+
+/**
+ * print_char - prints char
+ * @args: A va_list holding the next argument to the calling function.
+ * Return: The number of characters printed
+ */
+
+int print_char(va_list args)
+{
+	char charac = va_arg(args, int);
+        return (write(1, &charac, 1));
 }
 
 /**
 * print_string - Writes a string to stdout.
 * @args: A va_list holding the next argument to the calling function.
-*
-* Return: The number of characters printed.
+* Return: The number of characters printed
 */
 
 int print_string(va_list args)
 {
-char *str = va_arg(args, char *);
+	char *str = va_arg(args, char *);
 
-return (write(1, str, _strlen(str)));
+	return (write(1, str, _strlen(str)));
+}
+
+/**
+ * print_prcent - prints % to stdout
+ * @args - @args: A va_list holding the next argument to the calling function.
+ * Return: The number of characters printed
+ */
+
+int print_percent()
+{
+	char p = '%';
+	return (write(1, &p, 1));
 }
 
 /**
@@ -40,34 +66,36 @@ return (write(1, str, _strlen(str)));
 
 int print_number(va_list args)
 {
-char buffer[1024];
-int i = 0, j = 0, n = va_arg(args, int);
+	char buffer[1024];
+	int i = 0; /*J=0*/
+	int n = va_arg(args, int);
 
-if (n < 0)
-{
-buffer[i++] = '-';
-n *= -1;
-}
+	if (n < 0)
+	{
+		buffer[i++] = '-';
+		n *= -1;
+	}
 
-while (n > 0)
-{
-buffer[i++] = (n % 10) + '0';
-n /= 10;
-}
+	while (n > 0)
+	{
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
+	}
 
-if (i == 0)
-buffer[i++] = '0';
+	if (n == 0)
+		buffer[i++] = '0';
 
-buffer[i] = '\0';
+	buffer[i] = '\0';
+/*
+	while (j < i / 2)
+	{
+		char tmp = buffer[j];
 
-while (j < i / 2)
-{
-char tmp = buffer[j];
-buffer[j++] = buffer[i - j];
-buffer[i - j] = tmp;
-}
-
-return (write(1, buffer, _strlen(buffer)));
+		buffer[j++] = buffer[i - j];
+		buffer[i - j] = tmp;
+	}
+*/
+	return (write(1, buffer, _strlen(buffer)));
 }
 
 /**
@@ -79,40 +107,40 @@ return (write(1, buffer, _strlen(buffer)));
 
 int _printf(const char *format, ...)
 {
-int i = 0, j, count = 0;
-va_list args;
-fmt_t fmts[] = {
-{'s', print_string},
-{'d', print_number},
-{'i', print_number},
-{'\0', NULL}
-};
+	int i = 0, j, count = 0;
+	va_list args;
+	fmt_t fmts[] = {{'c', print_char},
+			{'s', print_string},
+			{'%', print_percent},
+			{'d', print_number},
+			{'i', print_number},
+			{'\0', NULL}};
 
-va_start(args, format);
+	va_start(args, format);
 
-while (format && format[i])
-{
-if (format[i] == '%')
-{
-j = 0;
-while (fmts[j].spec)
-{
-if (format[i + 1] == fmts[j].spec)
-{
-count += fmts[j].printer(args);
-i++;
-break;
-}
-j++;
-}
-if (!fmts[j].spec)
-count += write(1, &format[i], 1);
-}
-else
-count += write(1, &format[i], 1);
-i++;
-}
-va_end(args);
-return (count);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (fmts[j].type)
+			{
+				if (format[i + 1] == fmts[j].type)
+				{
+					count += fmts[j].func(args);
+					i++;
+					break;
+				}
+				j++;
+			}
+			if (!fmts[j].type)
+				count += write(1, &format[i], 1);
+			}
+		else
+			count += write(1, &format[i], 1);
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
 
--- main.h
+++ main.h
@@ -5,7 +5,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int _strlen(char *s);
+int print_string(va_list args);
+int print_number(va_list args);
 int _printf(const char *format, ...);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+        char type;
+        int (*func)(va_list);
+} fmt_t;
 
 #endif /* MAIN_H */
 
