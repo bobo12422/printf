ea537c24309f029fc7f4f17a6d0a402fa4b6b8e0
Bosco <bizimana922@gmail.com>
2023-03-28 04:00:33 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,120 +1,118 @@
 #include "main.h"
 
 /**
- * _printf - prints a formatted string to the standard output stream
- * @format: the format string to print
- *
- * Return: the number of characters printed (excluding the null byte used to
- *         end output to strings)
- */
-int _printf(const char *format, ...)
+* _strlen - Calculates the length of a string.
+* @s: The string to be measured.
+*
+* Return: The length of the string (excluding the terminating null byte).
+*/
+
+int _strlen(char *s)
 {
-	va_list args;
-	int i, j, count = 0;
-	print_t func_list[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_number},
-		{'i', print_number},
-		{'u', print_unsigned_number},
-		{'x', print_hexadecimal_lower},
-		{'X', print_hexadecimal_upper},
-		{'\0', NULL}
-	};
+int len = 0;
 
-	va_start(args, format);
+while (*s++)
+len++;
 
-	if (format == NULL)
-		return (-1);
-
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] != '%')
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		else
-		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			for (j = 0; func_list[j].type != '\0'; j++)
-			{
-				if (format[i + 1] == func_list[j].type)
-				{
-					count += func_list[j].func(args);
-					i++;
-					break;
-				}
-			}
-			if (func_list[j].type == '\0')
-			{
-				write(1, &format[i], 1);
-				count++;
-			}
-		}
-	}
-
-	va_end(args);
-
-	return (count);
+return (len);
 }
 
 /**
- * print_char - prints a character
- * @args: the va_list argument
- *
- * Return: the number of characters printed
- */
-int print_char(va_list args)
-{
-	char c = va_arg(args, int);
+* print_string - Writes a string to stdout.
+* @args: A va_list holding the next argument to the calling function.
+*
+* Return: The number of characters printed.
+*/
 
-	write(1, &c, 1);
-
-	return (1);
-}
-
-/**
- * print_string - prints a string
- * @args: the va_list argument
- *
- * Return: the number of characters printed
- */
 int print_string(va_list args)
 {
-	char *str = va_arg(args, char *);
+char *str = va_arg(args, char *);
 
-	if (str == NULL)
-		str = "(null)";
-
-	return (write(1, str, _strlen(str)));
+return (write(1, str, _strlen(str)));
 }
 
 /**
- * print_percent - prints a percent character
- * @args: the va_list argument (unused)
- *
- * Return: the number of characters printed
- */
-int print_percent(va_list args)
-{
-	(void)args;
+* print_number - Prints an integer to stdout.
+* @args: A va_list holding the next argument to the calling function.
+*
+* Return: The number of characters printed.
+*/
 
-	return (write(1, "%", 1));
-}
-
-/**
- * print_number - prints a signed integer
- * @args: the va_list argument
- *
- * Return: the number of characters printed
- */
 int print_number(va_list args)
 {
-	char buffer[1024];
-	int i = 0, j = 0, n = va_arg(args, int);
+char buffer[1024];
+int i = 0, j = 0, n = va_arg(args, int);
 
-	if (n
+if (n < 0)
+{
+buffer[i++] = '-';
+n *= -1;
+}
+
+while (n > 0)
+{
+buffer[i++] = (n % 10) + '0';
+n /= 10;
+}
+
+if (i == 0)
+buffer[i++] = '0';
+
+buffer[i] = '\0';
+
+while (j < i / 2)
+{
+char tmp = buffer[j];
+buffer[j++] = buffer[i - j];
+buffer[i - j] = tmp;
+}
+
+return (write(1, buffer, _strlen(buffer)));
+}
+
+/**
+* _printf - Prints a formatted string to stdout.
+* @format: The string to be formatted.
+*
+* Return: The number of characters printed.
+*/
+
+int _printf(const char *format, ...)
+{
+int i = 0, j, count = 0;
+va_list args;
+fmt_t fmts[] = {
+{'s', print_string},
+{'d', print_number},
+{'i', print_number},
+{'\0', NULL}
+};
+
+va_start(args, format);
+
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+j = 0;
+while (fmts[j].spec)
+{
+if (format[i + 1] == fmts[j].spec)
+{
+count += fmts[j].printer(args);
+i++;
+break;
+}
+j++;
+}
+if (!fmts[j].spec)
+count += write(1, &format[i], 1);
+}
+else
+count += write(1, &format[i], 1);
+i++;
+}
+va_end(args);
+return (count);
+}
 
--- main.h
+++ main.h
@@ -5,25 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/**
- * struct print - struct for print functions
- * @type: the type of the argument
- * @func: the print function
- */
-typedef struct print
-{
-	char type;
-	int (*func)(va_list);
-} print_t;
-
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_number(va_list args);
-int print_unsigned_number(va_list args);
-int print_hexadecimal_lower(va_list args);
-int print_hexadecimal_upper(va_list args);
 
 #endif /* MAIN_H */
 
